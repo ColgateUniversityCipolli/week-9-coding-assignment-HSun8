f5ccb96b12023fa4cfe533e4e9bf60ffac2a9bcb
HSun8 <24sunh@gmail.com>
2025-03-27 23:33:25 -0400

    weibull + plotting pdfs

--- HW9.R
+++ HW9.R
@@ -60,3 +60,40 @@ lognorm.mean <- MLEs.lognorm$par[1]
 lognorm.sd <- MLEs.lognorm$par[2]
 
 # compare to weibull 
+llweibull <- function(par, data, neg=F){
+  # a <- par[1]
+  # sigma <- par[2]
+  alpha <- exp(par[1]) # go from (-inf,inf) to (0,inf)
+  sigma <- exp(par[2]) # go from (-inf,inf) to (0,inf)
+  
+  lweibull <- sum(log(dweibull(x=data, shape=alpha, scale=sigma)), na.rm=T)
+  
+  return(ifelse(neg, -lweibull, lweibull))
+}
+
+MLEs.weibull <- optim(fn = llweibull,
+                      par = c(1,1),
+                      data = precipitation.long$Precipitation,
+                      neg=T)
+
+(MLEs.weibull$par <- exp(MLEs.weibull$par)) # transform
+weibull.alpha <- MLEs.weibull$par[1]
+weibull.sigma <- MLEs.weibull$par[2]
+
+# generating pdfs to check plots
+pdfs <- tibble(x = seq(0,13,length.out=1000)) |>
+  mutate(gamma.pdf = dgamma(x=x, shape = gamma.alpha, scale = gamma.sigma),
+         lognorm.pdf = dlnorm(x=x, meanlog = lognorm.mean, sdlog = lognorm.sd),
+         weibull.pdf = dweibull(x=x, shape = weibull.alpha, scale = weibull.sigma))
+
+# good enough...
+ggplot() +
+  geom_histogram(data=precipitation.long,
+                 aes(x=Precipitation, y=after_stat(density)),
+                 breaks = seq(0,13,1)) +
+  geom_line(data = pdfs, aes(x=x, y= gamma.pdf, color = "gamma"))+
+  geom_line(data = pdfs, aes(x=x, y=lognorm.pdf, color = "lognorm"))+
+  geom_line(data = pdfs, aes(x=x, y=weibull.pdf, color = "weibull"))
+
+
+
