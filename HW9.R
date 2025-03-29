f9ad1726c0b493e2b634a9f0b381d300d70c66f9
HSun8 <24sunh@gmail.com>
2025-03-28 21:10:42 -0400

    likelihood ratios done

--- HW9.R
+++ HW9.R
@@ -39,11 +39,11 @@ gamma.sigma <- MLEs.gamma$par[2]
 # lognormal dist
 lllognorm <- function(par, data, neg = F){
   # parameters
-  mean <- exp(par[1])
-  sd <- exp(par[2])
+  mu <- exp(par[1])
+  sigma <- exp(par[2])
   #mean <- par[1]
   #sd <- par[2]
-  llognorm <- sum(log(dlnorm(x=data, meanlog = mean, sdlog = sd)),na.rm=T)
+  llognorm <- sum(log(dlnorm(x=data, meanlog = mu, sdlog = sigma)),na.rm=T)
   
   return(ifelse(neg, -llognorm, llognorm))
 }
@@ -56,16 +56,15 @@ MLEs.lognorm <- optim(fn = lllognorm,
                       data = precipitation.long$Precipitation,
                       neg = T)
 (MLEs.lognorm$par <- exp(MLEs.lognorm$par)) # transform
-lognorm.mean <- MLEs.lognorm$par[1]
-lognorm.sd <- MLEs.lognorm$par[2]
+lognorm.mu <- MLEs.lognorm$par[1]
+lognorm.sigma <- MLEs.lognorm$par[2]
 
 # compare to weibull 
 llweibull <- function(par, data, neg=F){
-  # a <- par[1]
-  # sigma <- par[2]
   alpha <- exp(par[1]) # go from (-inf,inf) to (0,inf)
   sigma <- exp(par[2]) # go from (-inf,inf) to (0,inf)
-  
+  # alpha <- par[1]
+  # sigma <- par[2]
   lweibull <- sum(log(dweibull(x=data, shape=alpha, scale=sigma)), na.rm=T)
   
   return(ifelse(neg, -lweibull, lweibull))
@@ -83,7 +82,7 @@ weibull.sigma <- MLEs.weibull$par[2]
 # generating pdfs to check plots
 pdfs <- tibble(x = seq(0,13,length.out=1000)) |>
   mutate(gamma.pdf = dgamma(x=x, shape = gamma.alpha, scale = gamma.sigma),
-         lognorm.pdf = dlnorm(x=x, meanlog = lognorm.mean, sdlog = lognorm.sd),
+         lognorm.pdf = dlnorm(x=x, meanlog = lognorm.mu, sdlog = lognorm.sigma),
          weibull.pdf = dweibull(x=x, shape = weibull.alpha, scale = weibull.sigma))
 
 # good enough...
@@ -96,4 +95,28 @@ ggplot() +
   geom_line(data = pdfs, aes(x=x, y=weibull.pdf, color = "weibull"))
 
 
+# as we want the realized likelihood using the parameters found with MLE,
+# we used log() to cancel out the exp() to use the actual parameters in the
+# realized likelihood
+
+# parts c-e calculations
+weibull.ll <-  llweibull(par = log(c(weibull.alpha,weibull.sigma)),
+                              data = precipitation.long$Precipitation, neg = F)
+
+gamma.ll <- llgamma(par = log(c(gamma.alpha, gamma.sigma)), 
+                    data = precipitation.long$Precipitation, neg = F)
+
+lognorm.ll <- lllognorm(par = log(c(lognorm.mu, lognorm.sigma)),
+                        data = precipitation.long$Precipitation, neg = F) 
+
+# q1 < 1, so the gamma distribution has a better fit
+partc.ratio <- exp(weibull.ll - gamma.ll)
+
+# q2 > 1, so the weibull distribution has a better fit
+partd.ratio <- exp(weibull.ll - lognorm.ll)
+
+# q3 > 1, so the gamma distribution has a better fit 
+parte.ratio <- exp(gamma.ll - lognorm.ll)
+
+# if you look at the graph, it makes more
 
